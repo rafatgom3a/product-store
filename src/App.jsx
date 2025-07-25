14019e8fe31b48cc89aa021030f14a32eb3ee64b
rafatgomaa <rafatgomaa815@gmail.com>
2025-07-25 18:45:10 +0300

    Setup project structure

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+function App() {
+  return (
+    <div className="w-screen h-screen flex items-center justify-center bg-gray-100">
+      <h1 className="text-3xl font-bold text-blue-600 text-center">
+        Welcome to the Product Store
+      </h1>
+    </div>
+  );
+}
+
+export default App;
